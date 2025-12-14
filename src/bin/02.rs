dd42f32d9eb957fe2155cf263a52b7ef63a8bb5f
Allen Gueco <allen.gueco1@gmail.com>
2025-12-14 17:40:52 -0500

    day 2 part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,11 +1,95 @@
 advent_of_code::solution!(2);
 
+struct IdRange {
+    start: u64,
+    end: u64,
+}
+
+impl IdRange {
+    fn from_input(input: &str) -> Self {
+        let mut s = input.split('-');
+        let l = s.next().unwrap().parse::<u64>().unwrap();
+        let r = s.next().unwrap().parse::<u64>().unwrap();
+
+        IdRange { start: l, end: r }
+    }
+
+    fn get_invalid_ranges(&self) -> Vec<u64> {
+        let mut result: Vec<u64> = vec![];
+        for r in self.start..=self.end {
+            if Self::is_invalid(r) {
+                result.push(r);
+            }
+        }
+        result
+    }
+
+    fn get_invalid_ranges_part_2(&self) -> Vec<u64> {
+        let mut result: Vec<u64> = vec![];
+        for r in self.start..=self.end {
+            if Self::is_invalid(r) || Self::repeats_at_least_2(r) {
+                result.push(r);
+            }
+        }
+
+        if result.len() < 2 {
+            return vec![];
+        }
+
+        result
+    }
+
+
+    fn is_invalid(id: u64) -> bool {
+        let id = id.to_string();
+
+        if !id.len().is_multiple_of(2) {
+            return false;
+        }
+
+        let half_index = id.len() / 2;
+
+        let (first, second) = id.split_at(half_index);
+
+        first == second
+    }
+
+    fn repeats_at_least_2(id: u64) -> bool {
+        let id = id.to_string();
+
+        let mut section = String::new();
+
+        for c in id.chars() {
+
+        }
+        let half_index = id.len() / 2;
+
+        let (first, second) = id.split_at(half_index);
+
+        first == second
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let result = input
+        .trim()
+        .split(',')
+        .map(IdRange::from_input)
+        .flat_map(|range| range.get_invalid_ranges())
+        .sum();
+
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let result = input
+        .trim()
+        .split(',')
+        .map(IdRange::from_input)
+        .flat_map(|range| range.get_invalid_ranges_part_2())
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
